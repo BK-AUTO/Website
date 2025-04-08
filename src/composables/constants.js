8c29578fe156aee1bc4749d86c131966d923ad2e
vanhpham <pvietanh05@gmail.com>
2025-04-08 20:45:01 +0700

    [FE] update BASE_URL

--- src/composables/constants.js
+++ src/composables/constants.js
@@ -1,7 +1,7 @@
 export const PASSWORD_REGEX = new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,}$/);
 export const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
-export const BASE_URL = "http://127.0.0.1:9099/api"; 
+export const BASE_URL = "https://bkauto.vn/api"; 
 export const MANAGE_URL_PREFIX = "/manage"; 
 
 export const LANG_VN = "vi";
